f118cfc3dcc73a8c219438a89d5bdd12c138bdd3
Ryan Malecky <rmalecky@mediamath.com>
2016-09-24 19:40:27 -0400

    checks to make sure a failing test w/docker still fails the build

--- test/controllers/health_check_controller_test.rb
+++ test/controllers/health_check_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class HealthChecksControllerTest < ActionDispatch::IntegrationTest
   test "should get the git revision" do
     get health_check_url
-    assert_response(:success)
+    assert_response(:cats)
     assert_match(/{"last_commit":"[a-z0-9]{40}"}/, @response.body)
   end
 end
